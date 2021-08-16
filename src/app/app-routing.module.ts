287ff2b979bc1aa54a9013adc5a0a116799277b3
Moeketsi Tsotetsi <tmoeketsid@gmail.com>
2021-08-16 05:01:58 +0200

    app done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { DriversComponent } from './drivers/drivers.component';
+import { TeamsComponent } from './teams/teams.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'teams',component:TeamsComponent},
+  {path:'drivers',component:DriversComponent},
+  {path:'leaderboard',component:LeaderboardComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
+import { DriversComponent } from './drivers/drivers.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { TeamsComponent } from './teams/teams.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HomeComponent,
+    DriversComponent,
+    LeaderboardComponent,
+    TeamsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MainComponent]
 })
 export class AppModule { }
